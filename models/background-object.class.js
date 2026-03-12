64c057630070f819e7fb49fb3b86bc2949a2e14b
Can <cantpts@outlook.de>
2026-03-12 08:46:45 +0100

    feat: add heaven

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -3,7 +3,7 @@ import { MovableObject } from "./movable-object.class.js";
 export class BackgroundObject extends MovableObject {
 
     width = 720;
-    height = 400;
+    height = 480;
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
