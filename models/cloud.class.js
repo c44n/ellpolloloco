a55523bdd20ba8137ad84ee68437eaa3ced6e76b
Can <cantpts@outlook.de>
2026-03-12 23:47:37 +0100

    feat: add clouds animation

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -9,6 +9,12 @@ export class Cloud extends MovableObject {
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
-        
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.2;
+        }, 1000 / 60);
     }
 }
