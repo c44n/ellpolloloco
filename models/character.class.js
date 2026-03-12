715b79bc139d27597220d3c6a5806bbff6368823
Can <cantpts@outlook.de>
2026-03-12 08:50:26 +0100

    feat: optimize objects sizes

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,8 @@
 import { MovableObject } from "./movable-object.class.js";
 
 export class Character extends MovableObject {
+    height = 280;
+    y = 155;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,9 @@
 import { MovableObject } from "./movable-object.class.js";
 
 export class Chicken extends MovableObject {
-    y = 295;
+    height = 60;
+    width = 60;
+    y = 365;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
